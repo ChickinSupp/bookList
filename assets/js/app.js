08d0e89865c75c837ba082b246eb4edcb3e905db
O Padeiro <daviiddamiianbaker@gmail.com>
2020-06-21 12:45:53 -0400

    creating book class for later use

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// BOOK CLASS: REPRESENTS A BOOK
+class Book {
+    constructor(title,author,isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI CLASS: HANDLES UI TASKS
+
+// STORE CLASS: HANDLES STORAGE
+
+// EVENT: DISPLAY BOOKS
+
+// EVEN: ADD BOOK
+
+// EVENT: DELETE BOOK
