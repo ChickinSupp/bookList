9fd199db0458938ea3c45e9e0e693ffb82475520
O Padeiro <daviiddamiianbaker@gmail.com>
2020-06-21 13:24:57 -0400

    adding books to DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,45 @@ class Book {
 }
 
 // UI CLASS: HANDLES UI TASKS
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '45545'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '45545'
+            }
+        ];
+        const books = StoredBooks;
+
+        books.forEach( (book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.getElementById('book-list');
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href='#' class='btn btn-danger btn-sm delete'>X</a></a></td>`;
+
+        list.appendChild(row);
+    }
+}
 
 // STORE CLASS: HANDLES STORAGE
 
 // EVENT: DISPLAY BOOKS
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+console.log(UI.displayBooks);   
 
-// EVEN: ADD BOOK
+// EVENT: ADD BOOK
 
 // EVENT: DELETE BOOK
